b1535eb066ceb5bafab6c2c0766e7f00ce33b7bf
Chandrika Guntur <cguntur@gcsdesign.com>
2020-07-09 16:53:14 -0700

    Working on getting all businesses connected to the requested email address

--- includes/cdash_data_export.php
+++ includes/cdash_data_export.php
@@ -1,15 +1,98 @@
 <?php
-add_filter( 'wp_privacy_personal_data_exporters', 'cdash_register_exporter', 10);
-function cdash_register_exporter( $exporters_array ) {
-	$exporters_array['chamber-dashboard-business-directory'] = array(
-		'exporter_friendly_name' => 'CD Business exporter', // isn't shown anywhere
-		'callback' => 'cdash_exporter_function', // name of the callback function which is below
+add_filter( 'wp_privacy_personal_data_exporters', 'cdash_register_people_exporter', 10);
+function cdash_register_people_exporter( $exporters_array ) {
+	$exporters_array['chamber-dashboard-people'] = array(
+		'exporter_friendly_name' => 'CD People exporter', //isn't shown anywhere
+		'callback' => 'cdash_people_exporter_function', //name of the callback function which is below
 	);
 	return $exporters_array;
 }
 
-function cdash_exporter_function($email_address, $page = 1){
-    // Limit us to 500 at a time to avoid timing out.
+add_filter( 'wp_privacy_personal_data_exporters', 'cdash_register_business_exporter', 10);
+function cdash_register_business_exporter( $exporters_array ) {
+	$exporters_array['chamber-dashboard-business-directory'] = array(
+		'exporter_friendly_name' => 'CD Business exporter', //isn't shown anywhere
+		'callback' => 'cdash_business_exporter_function', //name of the callback function which is below
+	);
+	return $exporters_array;
+}
+
+
+function cdash_people_exporter_function($email_address, $page = 1){
+	// Limit us to 500 at a time to avoid timing out.
+	$number = 500;
+	$page   = (int) $page;
+	
+	global $person_metabox;
+
+	$export_items = array();
+	//Check if there are any prople records connected to the user with the given email
+
+	$user = get_user_by( 'email', $email_address );
+	$user_id = $user->ID;
+
+	cd_debug("User ID: " . $user_id);
+
+	$person_id = cdash_get_person_id_from_user_id($user_id, true);
+
+	cd_debug("Person ID: " . $person_id);
+
+	global $person_metabox;
+	$person_details = array();
+
+	$options = get_option( 'cdcrm_options' );
+
+	// Find connected posts
+	$people = get_posts( array(
+		'connected_type' => 'people_to_user',
+	  	'connected_items' => get_queried_object(),
+	  	'nopaging' => true,
+		'suppress_filters' => false
+	) );
+	if($people){
+        foreach ( (array) $people as $person ){
+			cd_debug("Person ID: " . $person->ID);
+			$person_meta = $person_metabox->the_meta($person->ID);
+			$person_details = cdash_get_person_data($person, $person_meta);
+
+			$data = array(
+				array(
+					'name' => __('Name', 'cdash'),
+					'value' => $person->post_title
+				),
+				array(
+					'name' => __('Emails', 'cdash'),
+					'value' => $person_details['email']
+				),
+				array(
+					'name' => __('Phone Numbers', 'cdash'),
+					'value' => $person_details['phone']
+				),
+				array(
+					'name' => __('Address', 'cdash'),
+					'value' => $person_details['address']
+				),
+			);
+
+			$export_items[] = array(
+                'group_id' => 'cd_people',
+				'group_label' => __('People', 'cdash'),
+				'group_description' => __( 'User&#8217;s Chamber Dashboard People data.', 'cdash' ),
+                'item_id' => 'person-'.$person->ID,
+                'data' => $data
+           );
+		}
+	}
+		
+	$done = count( $people ) < $number;
+	return array(
+		'data' => $export_items,
+		'done' => $done,
+	);
+}
+
+function cdash_business_exporter_function($email_address, $page = 1){
+	// Limit us to 500 at a time to avoid timing out.
 	$number = 500;
     $page   = (int) $page;
     
@@ -30,29 +113,24 @@ function cdash_exporter_function($email_address, $page = 1){
     
     if($businesses){
 		global $buscontact_metabox;
+		global $billing_metabox;
+    	
 		
         foreach ( (array) $businesses as $business ){
 			$contactmeta = $buscontact_metabox->the_meta($business->ID);
+			$billing_meta = $billing_metabox->the_meta($business->ID);
 			
 			$business_details = cdash_export_bus_location_data($contactmeta);
-			$people_details = cdash_get_people_data();
-			//Maybe get the person id from the user id and use that to get the person details
-
-			cd_debug("People Details: " . print_r($people_details, true));
-			
+   
             // here you can specify the fields, that exist in any way
 			$data = array(
-				array(
-					'name' => 'Business ID',
-					'value' => $business->ID
-				),
 				array(
 					'name' => 'Business Name',
 					'value' => $business->post_title
 				),
 				array(
-					'name' => 'Email',
-					'value' => $email_address
+					'name' => 'Billing Email',
+					'value' => $billing_meta['billing_email']
 				),
 				array(
 					'name' => 'Address',
@@ -66,10 +144,6 @@ function cdash_exporter_function($email_address, $page = 1){
 					'name' => 'Email',
 					'value' => $business_details['email']
 				),
-				array(
-					'name' => 'Connected Person Details',
-					'value' => $people_details['name']
-				),
             );
             $export_items[] = array(
                 'group_id' => 'businesses',
@@ -77,15 +151,16 @@ function cdash_exporter_function($email_address, $page = 1){
                 'item_id' => 'business-'.$business->ID,
                 'data' => $data
            );
-        }
-    }
-    // $done identifies whether or not the number of $comments is less than $number. If it is, all comments have been processed and we're done.
+        }	
+}
+	// $done identifies whether or not the number of $comments is less than $number. If it is, all comments have been processed and we're done.
 	$done = count( $businesses ) < $number;
 	// The function should return an array with the (array)'data' to be exported and whether or not we're (bool)'done'
 	return array(
 		'data' => $export_items,
 		'done' => $done,
 	);
+
 }
 
 function cdash_export_bus_location_data($contactmeta){
@@ -133,58 +208,104 @@ function cdash_export_bus_location_data($contactmeta){
 				if(is_array($phones)) {
 					foreach($phones as $phone) {
 						if(isset($phone['phonenumber'])){
-							$phone_number .= $phone['phonenumber'];
+							$phone_number .= $phone['phonenumber'] . ', ';
 						}
 					}
+					$phone_number = rtrim($phone_number, ', ');
 				}
 			}else{
 				$phone_number = '';
 			}
 			$business_details['phone'] = $phone_number;
 
+			$email_address = '';
 			if(isset($location['email'])) {
 				$emails = $location['email'];
 				if(is_array($emails)) {
 					foreach($emails as $email) {
 						if(isset($email['emailaddress'])){
-							$email = $email['emailaddress'];
+							$email_address .= $email['emailaddress'] . ', ';
 						}
 					}
+					$email_address = rtrim($email_address, ', ');
 				}
 			}else{
-				$email = '';
+				$email_address = '';
 			}
-			$business_details['email'] = $email;
+			$business_details['email'] = $email_address;
 		}
 	}
 	return $business_details;
 }
 
-function cdash_get_people_data(){
+function cdash_get_person_data($person, $person_meta){
 	$person_details = array();
-	cd_debug("Inside the get people function.<br />");
-
-	$options = get_option( 'cdcrm_options' );
-
-	// Find connected posts
-	$people = get_posts( array(
-		'connected_type' => 'businesses_to_people',
-	  	'connected_items' => get_queried_object(),
-	  	'nopaging' => true,
-		'suppress_filters' => false
-	) );
-	cd_debug("People: " . print_r($people, true));
-
-	if($people){
-        foreach ( (array) $people as $person ){
-			cd_debug("Person ID: " . $person->ID . "<br />");
-			$person_meta = $person_metabox->the_meta($person->ID);
-			if(isset($person_meta['title'])){
-				$person_details['name'] = $person_meta['title'];
-			}
-		}
+	$email_address = '';
+	$phone_numbers = '';
+	if(isset($person->title) && ''!= $person->title){
+		$person_details['name'] = $person->title;
+	}else{
+		$person_details['name'] = '';
 	}
-	return $person_details();
+
+	if(isset($person_meta['email']) && '' != $person_meta['email']){
+		$emails = $person_meta['email'];
+		if(is_array($emails)){
+			foreach($emails as $email){
+				if(isset($email['emailaddress'])){
+					$email_address .= $email['emailaddress'] . ', ';
+				}
+			}
+			$email_address = rtrim($email_address, ', ');
+		}
+	}else{
+		$email_address = '';
+	}
+	$person_details['email'] = $email_address;
+
+	if(isset($person_meta['phone']) && '' != $person_meta['phone']){
+		$phones = $person_meta['phone'];
+		if(is_array($phones)){
+			foreach($phones as $phone){
+				if(isset($phone['phonenumber'])){
+					$phone_numbers .= $phone['phonenumber'] . ', ';
+				}
+			}
+			$phone_numbers = rtrim($phone_numbers, ', ');
+		}
+	}else{
+		$phone_numbers = '';
+	}
+	$person_details['phone'] = $phone_numbers;
+
+	if( isset( $person_meta['address'] ) && '' !== $person_meta['address'] ) {
+		$person_address = $person_meta['address']; 
+	}else{
+		$person_address = '';
+	}
+
+	if( isset( $person_meta['city'] ) && '' !== $person_meta['city'] ) {
+		$person_city = $person_meta['city']; 
+	}else{
+		$person_city = '';
+	}
+
+	if( isset( $person_meta['state'] ) && '' !== $person_meta['state'] ) {
+		$person_state = $person_meta['state']; 
+	}else{
+		$person_state = '';
+	}
+
+	if( isset( $person_meta['zip'] ) && '' !== $person_meta['zip'] ) {
+		$person_zip = $person_meta['zip']; 
+	}else{
+		$person_zip = '';
+	}
+
+	$person_address_details = $person_address . ', ' . $person_city . ', ' . $person_state . ' ' . $person_zip;
+	$person_details['address'] = $person_address_details;
+
+	return $person_details;
 }
 
 
