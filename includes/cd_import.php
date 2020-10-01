41787dc0422001bcb2bdb65e8fc65d3595fa196c
Chandrika Guntur <cguntur@gcsdesign.com>
2020-10-01 09:47:29 -0700

    Included the jquery styles in the plugin and added data sanitization in the import file

--- includes/cd_import.php
+++ includes/cd_import.php
@@ -7,7 +7,7 @@ function cdash_import_form() { ?>
 		<h4><?php _e( 'Import Businesses', 'cdash' ); ?></h4>
 		<p><?php _e('Chamber Dashboard Business Directory plugin includes a free import feature. You can import businesses from a CSV file.  First, you must format the CSV properly.  Your CSV must have the following columns in the following order, even if some of the columns are empty: <ul><li>Business Name</li><li>Description</li><li>Category (separate multiple with semicolons)</li><li>Membership Level (separate multiple with semicolons)</li><li>Location Name</li><li>Address</li><li>City</li><li>State</li><li>Zip</li><li>Country</li><li>Business Hours</li><li>URL</li><li>Phone (separate multiple with semicolons)</li><li>Email (separate multiple with semicolons)</li></ul>', 'cdash'); ?></p>
 		<p><?php _e( 'Some programs format CSV files differently.  You might need to use either Google Drive or Open Office to save your CSV file so that it will upload correctly. Visit our <a href="https://chamberdashboard.com/docs/plugin-features/import-export/" target="_blank">documentation</a> pages for more information.', 'cdash' ); ?></p>
-		<p><a href="https://chamberdashboard.com/docs/plugin-features/import-export/#Free_Import_Option" target="_blank"><?php _e('Download a sample CSV to see how to format your file.', 'cdash'); ?></a></p>
+		<p><a href="https://chamberdashboard.com/docs/plugin-features/business-directory/add-import-listings/" target="_blank"><?php _e('Download a sample CSV to see how to format your file.', 'cdash'); ?></a></p>
 		<?php wp_import_upload_form('admin.php?page=cd-settings&tab=import_export'); ?>
 <?php
 
@@ -38,10 +38,16 @@ function cdash_import_form() { ?>
 		    	} else {
 		    		$row++;
 					// Get the post data
+					if(isset($data[0])){
+						$post_title = sanitize_text_field( $data[0] );
+					}
+					if(isset($data[1])){
+						$post_content = sanitize_textarea_field( $data[1] );
+					}
 					$businessinfo = array (
 						'post_type'     => 'business',
-						'post_title'    => sanitize_text_field($data[0]),
-						'post_content' 	=> sanitize_textarea_field($data[1]),
+						'post_title'    => $post_title,
+						'post_content' 	=> $post_content,
 						'post_status'   => 'publish',
 						);
 					// Create a business
@@ -77,7 +83,7 @@ function cdash_import_form() { ?>
 					// Get all the email addresses and put them in the array format wpalchemy expects
 					$emails = array();
 					if(isset($data[13]) && !empty($data[13])) {
-						$tempmails = explode(';', sanitize_text_field($data[13]));
+						$tempmails = explode(';', sanitize_email($data[13]));
 						foreach ($tempmails as $email) {
 							$emails[]['emailaddress'] = $email;
 						}
@@ -85,18 +91,50 @@ function cdash_import_form() { ?>
 						$emails = '';
 					}
 
+					//sanitize the input data
+					if(isset($data[4])){
+						$location_name = sanitize_text_field($data[4]);
+					}
+					if(isset($data[5])){
+						$street_address = sanitize_text_field($data[5]);
+					}
+
+					if(isset($data[6])){
+						$city = sanitize_text_field($data[6]);
+					}
+
+					if(isset($data[7])){
+						$state = sanitize_text_field($data[7]);
+					}
+
+					if(isset($data[8])){
+						$zip = sanitize_text_field($data[8]);
+					}
+
+					if(isset($data[9])){
+						$country = sanitize_text_field($data[9]);
+					}
+
+					if(isset($data[10])){
+						$hours = sanitize_text_field($data[10]);
+					}
+
+					if(isset($data[11])){
+						$url = esc_url($data[11]);
+					}
+
 					// Get the geolocation data
-					if( isset( $data[5] ) ) {
+					if( isset( $street_address ) ) {
 						// ask Google for the latitude and longitude
-						$rawaddress = sanitize_text_field($data[5]);
+						$rawaddress = $street_address;
 						if( isset( $data[6] ) ) {
-							$rawaddress .= ' ' . sanitize_text_field($data[6]);
+							$rawaddress .= ' ' . $city;
 						}
 						if( isset( $data[7] ) ) {
-							$rawaddress .= ' ' . sanitize_text_field($data[7]);
+							$rawaddress .= ' ' . $state;
 						}
 						if( isset( $data[8] ) ) {
-							$rawaddress .= ' ' . sanitize_text_field($data[8]);
+							$rawaddress .= ' ' . $zip;
 						}
 						$address = urlencode( $rawaddress );
 						$json = wp_remote_get(cdash_get_google_map_url($address));
@@ -113,18 +151,18 @@ function cdash_import_form() { ?>
 					// Create the array of location information for wpalchemy
 					$locationfields = array(
 							array(
-							'altname' 	=> $data[4],
-							'address'	=> $data[5],
-							'city'		=> $data[6],
-							'state'		=> $data[7],
-							'zip'		=> $data[8],
-              				'country'   => $data[9],
-              				'hours'     => sanitize_text_field($data[10]),
+							'altname' 	=> $location_name,
+							'address'	=> $street_address,
+							'city'		=> $city,
+							'state'		=> $state,
+							'zip'		=> $zip,
+              				'country'   => $country,
+              				'hours'     => $hours,
 							'latitude'	=> $latitude,
 							'longitude'	=> $longitude,
-							'url'		=> esc_url($data[11]),
-							'phone'		=> sanitize_text_field($numbers),
-							'email'		=> sanitize_email($emails),
+							'url'		=> $url,
+							'phone'		=> $numbers,
+							'email'		=> $emails,
 							)
 						);
 
